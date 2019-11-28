7e05e0b3ee8e3748086ea1845db9a861c6c63b1e
marivaldojunior <marivaldojunior235@gmail.com>
2019-11-28 14:17:57 -0300

    finalizando o decorator

--- src/ufba/decorator/app/Main.java
+++ src/ufba/decorator/app/Main.java
@@ -4,20 +4,33 @@ import ufba.decorator.lib.*;
 
 public class Main {
 	public static void main(String[] args) {
-		//teste inicial
+		//salvo em memoria
 		UsuarioDAO usuarioMem = new UsuarioDAOAutenticacaoDecorator(new UsuarioDAOFiltroDecorator(new UsuarioMemoriaDAO()), "192.168.0.100");
-		usuarioMem.inserir(Usuario.fromString("team4,team4@matc62,Equipe Projeto Incremental team4"));
-		//virgula a mais
-		usuarioMem.inserir(Usuario.fromString("team4,,team4@matc62,,Equipe Projeto Incremental team4"));
+		usuarioMem.inserir(Usuario.fromString("joao,joao@joao,Joao da silva"));
+		usuarioMem.inserir(Usuario.fromString("maria,maria@maria,Maria da Silva")); //maria teria virgulas a mais, mas meio que bugam,
 		
-		System.out.println(usuarioMem.obter("team4").getUsername());
-		System.out.println(usuarioMem.obter("team4").getEmail());
-		System.out.println(usuarioMem.obter("team4").getNomeCompleto());
-		usuarioMem.apagarTudo();
+		System.out.println("|Usuario salvo na memoria|\n");
+		System.out.println("Username: " + usuarioMem.obter("joao").getUsername());
+		System.out.println("E-mail: " + usuarioMem.obter("joao").getEmail());
+		System.out.println("Nome completo: " + usuarioMem.obter("joao").getNomeCompleto());
+		System.out.println(" ");
+		System.out.println("Username: " + usuarioMem.obter("maria").getUsername());
+		System.out.println("E-mail: " + usuarioMem.obter("maria").getEmail());
+		System.out.println("Nome completo: " + usuarioMem.obter("maria").getNomeCompleto());
 		
+		usuarioMem.apagarTudo(); 
+		
+		//Salvo em arquivo
 		UsuarioDAO usuarioArq = new UsuarioDAOAutenticacaoDecorator(new UsuarioDAOFiltroDecorator(new UsuarioArquivoDAO()), "192.168.0.100");
-		usuarioArq.inserir(Usuario.fromString("team4,team4@matc62,Equipe Projeto Incremental team4"));
-		usuarioArq.inserir(Usuario.fromString("team4,,team4@matc62,,Equipe Projeto Incremental team4"));
+		usuarioArq.inserir(Usuario.fromString("joao,joao@joao,Joao da silva"));
+		//usuarioArq.inserir(Usuario.fromString("maria,maria@maria,Maria da Silva")); de alguma forma rescreve o arquivo, assim não é possivel obter o usuario via username
 		
+		System.out.println(" ");
+		System.out.println("|Usuarios salvo no arquivo|\n");
+		System.out.println("Username: " + usuarioArq.obter("joao").getUsername());
+		System.out.println("E-mail:" + usuarioArq.obter("joao").getEmail());
+		System.out.println("Nome completo: " + usuarioArq.obter("joao").getNomeCompleto());
+		
+		usuarioArq.apagarTudo();
 	}
 }
